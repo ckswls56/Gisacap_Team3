174ecd6b0b414dc52391d9cec4fac6029377e0cf
Nachanjin <skxkswls@gmail.com>
2023-05-19 02:38:46 +0900

    Initial commit

---
+++ src/main/java/com/example/team3/Model/Office.java
@@ -0,0 +1,2 @@
+package com.example.team3.Model;public class Office {
+}
---
+++ src/test/java/com/example/team3/Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
