5bbdb5ba1235d93de3e2501c67e488eeb5987e6e
Nachanjin <skxkswls@gmail.com>
2023-05-19 03:31:49 +0900

    entity

--- src/main/java/com/example/team3/Model/GptOutput.java
+++ src/main/java/com/example/team3/Model/GptOutput.java
@@ -2,6 +2,7 @@ package com.example.team3.Model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
