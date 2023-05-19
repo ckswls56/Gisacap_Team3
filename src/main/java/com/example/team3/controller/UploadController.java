8493ef86dae680bd2904e450380c5c336262011a
gouyeonch <gouyeonch@naver.com>
2023-05-20 02:14:17 +0900

    iotUpload 추가

--- src/main/java/com/example/team3/controller/UploadController.java
+++ src/main/java/com/example/team3/controller/UploadController.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import com.example.team3.Model.ELK;
 import com.example.team3.Model.GPTParam;
 import com.example.team3.repository.GPTParamRepository;
+import com.example.team3.service.GPTParamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,6 +26,7 @@ public class UploadController {
 
     @Autowired
     GPTParamRepository gptParamRepository;
+    GPTParamService gptParamService;
     private static final String CURR_IMAGE_REPO_PATH = "C:\\upload";
 
     @RequestMapping(value="/upload", method=RequestMethod.POST)
@@ -34,8 +38,11 @@ public class UploadController {
         String path = CURR_IMAGE_REPO_PATH+"\\"+gptParam.getUserId()+"\\"+uploadTime.toLocalDate()+"_"+uploadTime.getHour()+"_"+uploadTime.getMinute()+"_"+uploadTime.getSecond();
         List fileList = fileProcess(multipartRequest,path);
 
+        ELK closestELKObject = gptParamService.findClosestELKObject(); // 현재시간과 가장 가까운 ELK 정보를 가져옴 (uploadTime을 그냥 사용할까 LocalDateTime 형식을 몰겠슴
+
         gptParam.setPhotoRef(path);
-        gptParam.setElkTime(uploadTime);
+        gptParam.setElkTime(closestELKObject.getElkTime());
+
         gptParamRepository.save(gptParam);
 
 
--- src/main/java/com/example/team3/service/GPTParamService.java
+++ src/main/java/com/example/team3/service/GPTParamService.java
@@ -1,7 +1,9 @@
 package com.example.team3.service;
 
+import com.example.team3.Model.ELK;
 import com.example.team3.Model.GPTParam;
 import com.example.team3.repository.GPTParamRepository;
+import com.example.team3.repository.IotUploadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,12 @@ import org.springframework.stereotype.Service;
 public class GPTParamService {
     @Autowired
     GPTParamRepository gptParamRepository;
+    IotUploadRepository iotUploadRepository;
 
     public void save(GPTParam gptParam){
         gptParamRepository.save(gptParam);
     }
-
+    public ELK findClosestELKObject() {
+        return iotUploadRepository.findFirstByOrderByElkTimeAsc();
+    }
 }
